fe9ea26978ad59e3428fe82c654a8554c85c42a0
3mero562-dev <3mero562@gmail.com>
2026-06-01 02:31:05 +0300

    Update main.py

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ app = FastAPI()
 
 VERIFY_TOKEN = "amer123"
 
+
 @app.get("/")
 def home():
     return {
@@ -11,15 +12,18 @@ def home():
         "message": "AI Bot Ready"
     }
 
-@app.get("/webhook")
-def verify_webhook(hub_mode: str = None,
-                   hub_verify_token: str = None,
-                   hub_challenge: str = None):
 
-    if hub_verify_token == VERIFY_TOKEN:
-        return int(hub_challenge)
+@app.get("/webhook")
+async def verify_webhook(request: Request):
+    mode = request.query_params.get("hub.mode")
+    token = request.query_params.get("hub.verify_token")
+    challenge = request.query_params.get("hub.challenge")
+
+    if mode == "subscribe" and token == VERIFY_TOKEN:
+        return int(challenge)
 
     return {"error": "Invalid token"}
+
 
 @app.post("/webhook")
 async def webhook(request: Request):
