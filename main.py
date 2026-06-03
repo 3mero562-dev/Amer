aca17b5f2be2d4f8d5ce8cec59c5bad13848b667
3mero562-dev <3mero562@gmail.com>
2026-06-03 14:20:27 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,45 @@
 from fastapi import FastAPI, Request
+import os
 
 app = FastAPI()
 
 VERIFY_TOKEN = "amer123"
 
+STORE_INFO = """
+أهلاً بك في كوكيز لارين 🍪
+
+⏰ أوقات العمل: من 3 عصراً إلى 10 مساءً
+
+- سخان كيكة كوكيز -
+• شخص واحد — 2500 د.ع
+• صغيرة (3-4 أشخاص) — 8000 د.ع
+• وسط (5-7 أشخاص) — 15000 د.ع
+• كبيرة (8-11 شخص) — 25000 د.ع
+
+- الكرواسون المحشي -
+شوكلا — 2000 د.ع
+لوتس — 2000 د.ع
+كراميل — 2000 د.ع
+
+- الدونات -
+نوتيلا بيضاء — 1000 د.ع
+نوتيلا — 1000 د.ع
+فراولة — 1000 د.ع
+
+- المشروبات -
+موهيتو
+بلو بيري
+ليمون نعناع
+صودا
+
+السعر 2500 د.ع
+
+🔥 الكمية محدودة يومياً
+"""
 
 @app.get("/")
 def home():
-    return {
-        "status": "working",
-        "message": "AI Bot Ready"
-    }
-
+    return {"status": "working"}
 
 @app.get("/webhook")
 async def verify_webhook(request: Request):
@@ -24,10 +52,11 @@ async def verify_webhook(request: Request):
 
     return {"error": "Invalid token"}
 
-
 @app.post("/webhook")
 async def webhook(request: Request):
     data = await request.json()
-    print(data)
-    return {"status": "ok"}
 
+    print("New Message:")
+    print(data)
+
+    return {"status": "ok"}
