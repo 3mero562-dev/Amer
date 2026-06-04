e5b44b458d706a621951be339d78b0812b5ca928
3mero562-dev <3mero562@gmail.com>
2026-06-04 14:44:57 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,81 +1,58 @@
-import json
 from fastapi import FastAPI, Request
+from fastapi.responses import PlainTextResponse
 import requests
-
-BOT_TOKEN ="8876951923:AAFCzlMvrasHtjh68mTau7vjRuBGXp7xBeM"
-CHAT_ID = "5805710703"
+import json
 
 app = FastAPI()
 
 VERIFY_TOKEN = "amer123"
 
-STORE_INFO = """
-أهلاً بك في كوكيز لارين 🍪
+BOT_TOKEN = "8876951923:AAFCzlMvrasHtjh68mTau7vjRuBGXp7xBeM"
+CHAT_ID = "5805710703"
 
-⏰ أوقات العمل: من 3 عصراً إلى 10 مساءً
 
-- سخان كيكة كوكيز -
-• شخص واحد — 2500 د.ع
-• صغيرة (3-4 أشخاص) — 8000 د.ع
-• وسط (5-7 أشخاص) — 15000 د.ع
-• كبيرة (8-11 شخص) — 25000 د.ع
+@app.get("/")
+async def home():
+    return {"status": "working"}
 
-- الكرواسون المحشي -
-شوكلا — 2000 د.ع
-لوتس — 2000 د.ع
-كراميل — 2000 د.ع
 
-- الدونات -
-نوتيلا بيضاء — 1000 د.ع
-نوتيلا — 1000 د.ع
-فراولة — 1000 د.ع
-
-- المشروبات -
-موهيتو
-بلو بيري
-ليمون نعناع
-صودا
-
-السعر 2500 د.ع
-
-🔥 الكمية محدودة يومياً
-"""
 @app.get("/webhook")
 async def verify_webhook(request: Request):
+
     mode = request.query_params.get("hub.mode")
     token = request.query_params.get("hub.verify_token")
     challenge = request.query_params.get("hub.challenge")
 
     if mode == "subscribe" and token == VERIFY_TOKEN:
-        return int(challenge)
+        return PlainTextResponse(content=challenge)
 
-    return {"error": "Invalid token"}
-
-@app.get("/")
-def home():
-    return {"status": "working"}
+    return {"error": "verification failed"}
 
 
 @app.post("/webhook")
 async def webhook(request: Request):
+
     data = await request.json()
 
-    print("\n\n========== NEW REQUEST ==========")
+    print("\n========== INSTAGRAM EVENT ==========")
     print(json.dumps(data, indent=4, ensure_ascii=False))
-    print("=================================\n\n")
+    print("=====================================\n")
 
     try:
-        text = json.dumps(data, indent=4, ensure_ascii=False)
-
         requests.post(
             f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage",
             json={
                 "chat_id": CHAT_ID,
-                "text": text[:4000]
-            }
+                "text": json.dumps(
+                    data,
+                    indent=2,
+                    ensure_ascii=False
+                )[:4000]
+            },
+            timeout=10
         )
 
     except Exception as e:
-        print("Telegram Error:", e)
+        print("TELEGRAM ERROR:", e)
 
     return {"status": "ok"}
