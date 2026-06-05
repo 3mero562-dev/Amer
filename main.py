9957502c193e730209bef7b7d07d3fb1587f1aa6
3mero562-dev <3mero562@gmail.com>
2026-06-05 19:12:12 +0300

    Update main.py

--- main.py
+++ main.py
@@ -99,20 +99,20 @@ async def webhook(request: Request):
 
 🚚 سيتم التوصيل خلال ساعتين من تأكيد الحجز"""
      
-product = user_orders.get(sender_id, "منتج غير محدد")
-
-telegram_message = f"""
-📦 طلب جديد من الانستكرام
-
-🍪 المنتج:
-{product}
-
-👤 User ID:
-{sender_id}
-
-📱 الرقم والعنوان:
-{message_text}
-"""
+            product = user_orders.get(sender_id, "منتج غير محدد")
+            
+            telegram_message = f"""
+            📦 طلب جديد من الانستكرام
+            
+            🍪 المنتج:
+            {product}
+            
+            👤 User ID:
+            {sender_id}
+            
+            📱 الرقم والعنوان:
+            {message_text}
+            """
 
 requests.post(
     f"https://api.telegram.org/bot{TELEGRAM_BOT_TOKEN}/sendMessage",
