64d7ff9394741ef77880b56323e77eff1c31a98e
3mero562-dev <3mero562@gmail.com>
2026-06-06 22:05:28 +0300

    Update main.py

--- main.py
+++ main.py
@@ -94,7 +94,7 @@ async def webhook(request: Request):
 
         hour = datetime.now().hour
         
-        if hour < 14 or hour >= 22:
+        if hour < 14 or hour >= 23:
             reply = """
         نعتذر منكم 🙏
         
