df137973c83032cbdc76763751111bfd8d01dc70
3mero562-dev <3mero562@gmail.com>
2026-06-08 01:08:55 +0300

    Update main.py

--- main.py
+++ main.py
@@ -188,7 +188,7 @@ async def webhook(request: Request):
     hour = (datetime.now() + timedelta(hours=3)).hour
     print("CURRENT HOUR =", hour)
 
-    if hour < 11 or hour >= 22:
+    if hour < 0 or hour >= 24:
         reply = """
 نعتذر منكم 🙏
 
