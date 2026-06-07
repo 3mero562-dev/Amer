f0d0c60579d617df801b20f75ef0797977889b4f
3mero562-dev <3mero562@gmail.com>
2026-06-07 03:32:09 +0300

    Update main.py

--- main.py
+++ main.py
@@ -111,8 +111,17 @@ async def webhook(request: Request):
         شكراً لاختياركم كوكيز لارين ❤️🍪
         """
        
+        meta_response = requests.post(
+        f"https://graph.instagram.com/v23.0/me/messages?access_token={INSTAGRAM_ACCESS_TOKEN}",
+        json={
+        "recipient": {"id": sender_id},
+        "message": {"text": reply}
+    },
+        timeout=10
+)
+
         print("CLOSED HOURS ACTIVE")
-            return {"status": "closed_hours"}
+        return {"status": "closed_hours"}
         print("SENDER =", event.get("sender"))
         print("RECIPIENT =", event.get("recipient"))
         if not sender_id or not message_text:
